760fa96160d41ff89cdb65eae4670d6d03f6a794
ACGreve <greve5921@gmail.com>
2023-07-10 20:31:03 -0400

    Added new bread form and button

--- views/index.jsx
+++ views/index.jsx
@@ -22,6 +22,13 @@ function Index ({ breads }) {
         <ul>
             {breadDisplay}
         </ul>
+        <div>
+            <a href = '/breads/new'>
+            <button>
+                Add a new bread
+            </button>
+            </a>
+        </div>
       </Default>
     )
 }
