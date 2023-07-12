31544a2a344d4d6a1d1e4963a1225df584d854a1
ACGreve <greve5921@gmail.com>
2023-07-12 19:41:30 -0400

    Added edit page

--- controllers/bread.js
+++ controllers/bread.js
@@ -18,6 +18,15 @@ router.get('/:index', (req,res) => {
     })
 })
 
+//GET edit page
+router.get('/:index/edit', (req,res) => {
+    const {index} = req.params
+    res.render('edit',{
+        bread:Bread[index],
+        index
+    })
+})
+
 router.post('/', (req, res) => {
     //it will be using a checkbox item from a form, and checking if it is "on"
     if (req.body.hasGluten === 'on'){
@@ -30,6 +39,20 @@ router.post('/', (req, res) => {
 
     Bread.push(req.body)
     res.status(303).redirect('/breads')
+})
+
+router.put('/:index', (req, res) => {
+    const { index } = req.params
+    if (req.body.hasGluten === 'on'){
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+
+    if (!req.body.image) req.body.image='https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
+
+    Bread[index]=req.body
+    res.status(303).redirect(`/breads/${index}`)
 })
 
 router.delete('/:index', (req, res) => {
