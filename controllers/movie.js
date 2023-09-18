1ee8fa4f8f47335307a72e653a89a7d3abf9796f
Brandon Life <blife1119@gmail.com>
2023-09-18 19:58:30 -0400

    fixed post and put routes

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,10 +1,12 @@
-const router = require('express').Router()
+const movies = require('express').Router()
+const movie = require('../models/movie')
 const Movie = require('../models/movie')
 const movieseedData = require('../models/movieSeedData')
 
 
 //Seed data route
-router.get('/data/seed', async (req, res)=>{
+movies.get('/data/seed', async (req, res)=>{
+  
   await Promise.all([Movie.deleteMany()])
   const movies = await Movie.insertMany(movieseedData)
   const movieIds = movies.map(movie => movie._id)
@@ -14,52 +16,53 @@ router.get('/data/seed', async (req, res)=>{
 
 
 //Home Route for all the movies
-router.get('/', async(req, res)=>{//this route works fine
+movies.get('/', async(req, res)=>{//this route works fine
   const movies = await Movie.find()
   res.render('index', {movies})
 })
 
+//Create a movie
+movies.post('/', async (req, res)=>{//this route needs work. It does post but without the body even though i put a subject in the body of postman
+  console.log("I was hit")
+  console.log(req.body)
+  let createdMovie = await Movie.create(req.body)
+  res.json(createdMovie)
+})
+
 //Get a specific movie page
-router.get('/:id', async (req, res)=>{//this route works fine
+movies.get('/:id', async (req, res)=>{//this route works fine
   const {id} = req.params
   const movie = await Movie.findById(id)
   res.render('movieShow', {movie})
   // res.json(movie)
 })
 
-//Create a movie
-router.post('/', async (req, res)=>{//this route needs work. It does post but without the body even though i put a subject in the body of postman
-  console.log(req.body)
-  let createdMovie = await Movie.create(req.body)
-  res.send(createdMovie)
-})
 
 //Render new movie page
-router.get('/newmovie', async (req, res)=>{
+movies.get('/newmovie', async (req, res)=>{
   const movies = await Movie.find()
   // res.render('newMovie', {movies})// newmovie needs to be created
 })
 
 //Get edit page for movie
-router.get('/:id/edit', async (req, res)=>{
+movies.get('/:id/edit', async (req, res)=>{
   const {id}= req.params
   let movie = await Movie.findById(id)
   // res.render('moviesEdit', {movie})// moviesEdit needs to be created
   res.json(movie)
 })
-
-router.delete('/:id', async (req, res)=>{
+movies.delete('/:id', async (req, res)=>{
   const {id} = req.params
  let deletedMovie = await Movie.findByIdAndDelete(id)
   // res.redirect('/movies')
   res.json(deletedMovie)
 })
 
-router.put('/:id', async (req, res)=>{
+movies.put('/:id', async (req, res)=>{
   const {id} = req.params
   let updatedMovie = await Movie.findByIdAndUpdate(id, req.body)
   // res.status(303).redirect(`/movies/${id}`)
   res.json(updatedMovie)
 })
 
-module.exports = router
+module.exports = movies
--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const express = require('express')
 const methodOverride = require('method-override')
 require('dotenv').config()
-const breadController = require('./controllers/bread')
-const bakerController = require('./controllers/baker')
-const movieController = require('./controllers/movie')
 const mongoose = require('mongoose')
 
 const app = express()
 
 // MIDDLEWARE
+app.use(express.json())
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-
 //routes
 // app.use('/breads', breadController)
 // app.use('/bakers', bakerController)
-app.use('/movies', movieController)
-
 // db connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('DB connected'))
     .catch(err => console.error(err));
+
+const movieController = require('./controllers/movie.js')
+app.use('/movies', movieController)
+
+
 
 const PORT= process.env.PORT
 
