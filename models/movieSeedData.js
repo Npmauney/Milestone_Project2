eb906f69e58317ab577386073fba79ffa1fe14e2
Brandon Life <blife1119@gmail.com>
2023-09-18 20:32:02 -0400

    changed poster_path string

--- models/movieSeedData.js
+++ models/movieSeedData.js
@@ -3,21 +3,21 @@ module.exports = [
   original_lanugage: "en",
   original_title: 'Up',
   overview: 'Carl Fredricksen spent his entire life dreaming of exploring the globe and experiencing life to its fullest. But at age 78, life seems to have passed him by, until a twist of fate (and a persistent 8-year old Wilderness Explorer named Russell) gives him a new lease on life.',
-  poster_path: 'vpbaStTMt8qqXaEgnOR2EE4DNJk.jpg',
+  poster_path: 'https://image.tmdb.org/t/p/original/vpbaStTMt8qqXaEgnOR2EE4DNJk.jpg',
   release_date: "2009-05-28"
 },
 {
   original_lanugage: "en",
   original_title: 'Up',
   overview: "Adolf Schwartz has been killed. Who did it? No-one knows or cares, as they're too busy being distracted by busty Margo Winchester, who hitch-hikes into town and gets involved with all the local men.",
-  poster_path: 'kU6wvyu8yxIf7IynQmn8P0Gi6fN.jpg',
+  poster_path: 'https://image.tmdb.org/t/p/original/kU6wvyu8yxIf7IynQmn8P0Gi6fN.jpg',
   release_date: "1976-10-01"
 },
 {
   original_lanugage: "en",
   original_title: 'Up',
   overview: 'Up is a 1984 American short film directed by Mike Hoover and Tim Huntley. The film depicts a man who sets a hawk free, then tries to find it in the wild on his hang glider. In 1985, it won an Oscar for Best Short Subject at the 57th Academy Awards.',
-  poster_path: '1A82pXCVQyZ9Hh1VteuKchhJr1B.jpg',
+  poster_path: '"https://image.tmdb.org/t/p/original/1A82pXCVQyZ9Hh1VteuKchhJr1B.jpg"',
   release_date: "1984-01-01"
 }
 ]
